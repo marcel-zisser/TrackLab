a69eb2875a2f4a81cf2d2e7c382c21e8ec1a68f2
Marcel Zisser <marcel.zisser@student.tugraz.at>
2025-05-11 21:38:29 +0200

    Adjusted header and footer

--- libs/shared/components/src/lib/footer/footer.component.ts
+++ libs/shared/components/src/lib/footer/footer.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Divider } from 'primeng/divider';
 
 @Component({
   selector: 'tl-footer',
-  imports: [Divider],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
