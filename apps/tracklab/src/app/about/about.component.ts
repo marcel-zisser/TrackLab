4dc892a04d733a2a78bb4ba4a26dad95b67aca4c
Marcel Zisser <marcel.zisser@student.tugraz.at>
2025-09-06 15:41:18 +0200

    Added About page

--- apps/tracklab/src/app/about/about.component.ts
+++ apps/tracklab/src/app/about/about.component.ts
@@ -1,11 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { InfoPageComponent } from '@tracklab/shared/components';
+import { Divider } from 'primeng/divider';
+import { NgOptimizedImage } from '@angular/common';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'tl-disclaimer',
-  imports: [InfoPageComponent],
+  imports: [InfoPageComponent, Divider, NgOptimizedImage],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutComponent {}
+export class AboutComponent {
+  protected readonly PrimeIcons = PrimeIcons;
+}
--- apps/tracklab/src/app/acknowledgements/acknowledgements.component.ts
+++ apps/tracklab/src/app/acknowledgements/acknowledgements.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { InfoPageComponent } from '@tracklab/shared/components';
+import { Divider } from 'primeng/divider';
 
 @Component({
   selector: 'tl-disclaimer',
-  imports: [InfoPageComponent],
+  imports: [InfoPageComponent, Divider],
   templateUrl: './acknowledgements.component.html',
   styleUrl: './acknowledgements.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
