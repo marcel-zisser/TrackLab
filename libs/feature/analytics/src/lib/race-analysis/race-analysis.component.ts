8b03507cb09190563e6fb5885c6f8edcf82cae48
Marcel Zisser <marcel.zisser@student.tugraz.at>
2025-09-27 23:26:14 +0200

    Removed unused imports

--- libs/feature/analytics/src/lib/race-analysis/race-analysis.component.ts
+++ libs/feature/analytics/src/lib/race-analysis/race-analysis.component.ts
@@ -1,13 +1,8 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RaceAnalysisStore } from './store/race-analysis.store';
-import { Menu } from 'primeng/menu';
-import { Ripple } from 'primeng/ripple';
-import { Badge } from 'primeng/badge';
-import { Avatar } from 'primeng/avatar';
 
 @Component({
   selector: 'tl-race-analysis',
-  imports: [Menu, Ripple, Badge, Avatar],
   providers: [RaceAnalysisStore],
   templateUrl: './race-analysis.component.html',
   styleUrl: './race-analysis.component.css',
